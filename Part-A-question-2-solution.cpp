e40e36928159b8bac17f123d76bfbd0129a624d1
PRANSHU331 <63441959+PRANSHU331@users.noreply.github.com>
2023-12-21 04:33:22 +0530

    Create Part-A-question-2-solution.cpp

---
+++ Part-A-question-2-solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    // Number of faces on each die
+    const int faces_A = 6;
+    const int faces_B = 6;
+
+    // Vector to store the distribution of sums
+    std::vector<int> sum_distribution(2 * faces_A + 1, 0);
+
+    // Calculate distribution
+    for (int face_A = 1; face_A <= faces_A; ++face_A) {
+        for (int face_B = 1; face_B <= faces_B; ++face_B) {
+            int sum = face_A + face_B;
+            sum_distribution[sum]++;
+        }
+    }
+
+    // Output the distribution
+    for (int i = 2; i <= 2 * faces_A; ++i) {
+        std::cout << "Sum " << i << ": " << sum_distribution[i] << " combinations" << std::endl;
+    }
+
+    return 0;
+}
